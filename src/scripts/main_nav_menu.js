e43f73f7f20d0d00551eb5e1cf6ff22df16c15ef
Sivasankaran K B <kbsiva7@gmail.com>
2025-04-17 17:44:04 +0530

    Focus on first link in nav menu on open
    Will help with a11y

--- src/scripts/main_nav_menu.js
+++ src/scripts/main_nav_menu.js
@@ -1,10 +1,12 @@
 (function setupMainNavMenu(){
     const menuOpenButton = document.getElementById('main-nav-menu-open')
     const menuContainer = document.getElementById('main-nav-menu')
+    const menuItem0 = document.getElementById('main-nav-menu-item-0')
     const menuCloseButton = document.getElementById('main-nav-menu-close')
 
     function showMainMenu(){
         menuContainer.classList.remove('hidden')
+        menuItem0.focus()
     }
 
     function hideMainMenu(){
